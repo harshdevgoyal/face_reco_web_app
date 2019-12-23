a9b308c24a7bb835df5c7a0159f8d566ec39b24e
Harsh <harshdevgoyal@gmail.com>
2019-12-23 13:59:59 +0530

    added docker

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM python:3.6-slim-stretch
+
+RUN apt-get -y update
+RUN apt-get install -y --fix-missing \
+    build-essential \
+    cmake \
+    gfortran \
+    git \
+    wget \
+    curl \
+    graphicsmagick \
+    libgraphicsmagick1-dev \
+    libatlas-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libgtk2.0-dev \
+    libjpeg-dev \
+    liblapack-dev \
+    libswscale-dev \
+    pkg-config \
+    python3-dev \
+    python3-numpy \
+    software-properties-common \
+    zip \
+    && apt-get clean && rm -rf /tmp/* /var/tmp/*
+
+RUN cd ~ && \
+    mkdir -p dlib && \
+    git clone -b 'v19.9' --single-branch https://github.com/davisking/dlib.git dlib/ && \
+    cd  dlib/ && \
+    python3 setup.py install --yes USE_AVX_INSTRUCTIONS
+
+
+
+#WORKDIR app 
+# Install pytorch and fastai
+#RUN pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+
+# ADD requirements.txt .
+# RUN pip install -r requirements.txt
+#pip install --no-cache-dir -r
+#ADD models models
+#ADD src src
+
+COPY . /root/face_recognition
+
+RUN cd /root/face_recognition && \
+    pip3 install -r requirements.txt
+
+ENV PORT=8000
+
+# Start the server
+CMD ["python", "src/app.py", "serve"]
--- src/app.py
+++ src/app.py
@@ -1,7 +1,4 @@
-
-#from fastai import *
-#from fastai.vision import *
-#import fastai
+import numpy as np
 import yaml
 import sys
 from io import BytesIO
@@ -9,70 +6,165 @@ from typing import List, Dict, Union, ByteString, Any
 import os
 import flask
 from flask import Flask
+#import matplotlib.pyplot as plt
 import requests
-#import torch
 import json
+import face_recognition
+from PIL import Image,ImageDraw
+
+
 
 with open("config.yaml", 'r') as stream:
     APP_CONFIG = yaml.full_load(stream)
 
+
 app = Flask(__name__)
 
+unknown_face_distances=None
+unknown_face_encodings=None
+known_face_encodings=[]
+known_face_names=[]
+unknown_image=None
 
-# def load_model(path=".", model_name="model.pkl"):
-#     learn = load_learner(path, fname=model_name)
-#     return learn
+def create_unknown_face_encodings(raw_bytes: ByteString):
+    global unknown_image
+    print(type(BytesIO(raw_bytes)))
+    unknown_image=face_recognition.load_image_file(BytesIO(raw_bytes))
+    unknown_face_locations = face_recognition.face_locations(unknown_image)
+    unknown_face_encodings=face_recognition.face_encodings(unknown_image,unknown_face_locations)
+    return unknown_face_locations,unknown_face_encodings
+
+def create_known_face_encodings(raw_bytes: ByteString):
+    known_image=face_recognition.load_image_file(BytesIO(raw_bytes))
+    known_face_encodings=face_recognition.face_encodings(known_image)
+    return known_face_encodings[0]
+
+def get_output():
+    pil_image = Image.fromarray(unknown_image)
+    # Create a Pillow ImageDraw Draw instance  to draw with
+    draw = ImageDraw.Draw(pil_image)
+    # Loop through each face found in the unknown image
+
+    for (top, right, bottom, left), face_encoding in zip(unknown_face_locations, unknown_face_encodings):
+
+        
+        # See if the face is a match for the known face(s)
+        #matches = face_recognition.face_distance(known_face_encodings, face_encoding)
+        
+        name = "__"
+
+        # Or instead, use the known face with the smallest distance to the new face
+
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+
+        # ind=np.argsort(face_distances)
+        # distances.append(face_distances[ind])
+        # names.append(np.array(known_face_names)[ind])
+        # indexes.append(i)
+
+        #if min(face_distances)<=0.55:
+        best_match_index = np.argmin(face_distances)
+        name = known_face_names[best_match_index]
+            
+        #if matches[best_match_index]:
+
+        # Draw a box around the face using the Pillow module
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+        # Draw a label with a name below the face
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+        
+    # Remove the drawing library from memory as per the Pillow docs
+    del draw
+
+    # Display the resulting image
+    #pil_image.save(directory+'output.jpg')
+
+    # b=BytesIO()
+    # pil_image=pil_image.resize((512,512))
+    # pil_image.save(b,'jpeg')
+    # return b
+    print(type(pil_image))
+    pil_image.save('./static/output.jpg')
+   #return flask.send_file(flask.url_for()'output.jpg',mimetype='image/jpg')
+    #display(pil_image)
 
 
-def load_image_url(url: str) -> Image:
-    response = requests.get(url)
-    img = open_image(BytesIO(response.content))
-    return img
+@app.route('/api/upload_group_pic', methods=['POST'])
+def upload_group_pic():
+    file=flask.request.files['file']
+    filename=file.filename
+    bytes = file.read()
+    # print(bytes)
+    global unknown_face_locations
+    global unknown_face_encodings
+    unknown_face_locations,unknown_face_encodings=create_unknown_face_encodings(bytes)
+    print(os.path.splitext(filename)[0])
+    print(f'No of unknown encodings:{len(unknown_face_encodings)}')
+    return f'No of unknown encodings:{len(unknown_face_encodings)}'
+
+@app.route('/api/upload_ind_pics', methods=['POST'])
+def upload_ind_pic():
+    files = flask.request.files.getlist('file')
+    # print(type(images[0]))
+    # print(images[0].read())
+   
+    global known_face_encodings
+    global known_face_names
+    known_face_encodings=[]
+    known_face_names=[]
+    for file in files:
+        filename=file.filename
+        bytes=file.read()
+        known_face_names.append(os.path.splitext(filename)[0])
+        known_face_encodings.append(create_known_face_encodings(bytes))
+   
+    print(f'No of known encodings:{len(known_face_encodings)}')
+    print(known_face_names)
+
+    return f'No of known encodings:{len(known_face_encodings)}'
 
 
-# def load_image_bytes(raw_bytes: ByteString) -> Image:
-#     img = open_image(BytesIO(raw_bytes))
-#     return img
+@app.route('/api/recognize')
+def recognize():
+
+    get_output()
+    #url=flask.url_for('static', filename='output.jpg')
+
+    #print(url)
+    return 'success'
+
+    # return flask.send_file(
+    # get_output(),
+    # mimetype='image/jpeg',
+    # as_attachment=True,
+    # attachment_filename='output.jpg')
+
+# @app.route('/api/classify', methods=['POST', 'GET'])
+# def upload_file():
+#     if flask.request.method == 'GET':
+#         url = flask.request.args.get("url")
+#         img = load_image_url(url)
+#     else:
+#         bytes = flask.request.files['file'].read()
+#         img = load_image_bytes(bytes)
+#     #res = predict(img)
+#     return flask.jsonify(res)
 
 
-# def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
-#     pred_class, pred_idx, outputs = model.predict(img)
-#     pred_probs = outputs / sum(outputs)
-#     pred_probs = pred_probs.tolist()
-#     predictions = []
-#     for image_class, output, prob in zip(model.data.classes, outputs.tolist(), pred_probs):
-#         output = round(output, 1)
-#         prob = round(prob, 2)
-#         predictions.append(
-#             {"class": image_class.replace("_", " "), "output": output, "prob": prob}
-#         )
-
-#     predictions = sorted(predictions, key=lambda x: x["output"], reverse=True)
-#     predictions = predictions[0:n]
-#     return {"class": str(pred_class), "predictions": predictions}
-
-
-@app.route('/api/classify', methods=['POST', 'GET'])
-def upload_file():
-    if flask.request.method == 'GET':
-        url = flask.request.args.get("url")
-        img = load_image_url(url)
-    else:
-        bytes = flask.request.files['file'].read()
-        img = load_image_bytes(bytes)
-    res = predict(img)
-    return flask.jsonify(res)
-
-
-@app.route('/api/classes', methods=['GET'])
-def classes():
-    classes = sorted(model.data.classes)
-    return flask.jsonify(classes)
+# @app.route('/api/classes', methods=['GET'])
+# def classes():
+#     #classes = sorted(model.data.classes)
+#     return flask.jsonify('f')
 
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return "pong"
+    # pil_image=Image.open('sample.jpg')
+    # pil_image.save('/static/output.jpg')
+    return os.getcwd()
+
 
 
 @app.route('/config')
@@ -107,7 +199,7 @@ def before_request():
     app.jinja_env.cache = {}
 
 
-model = load_model('models')
+#model = load_model('models')
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
@@ -115,5 +207,5 @@ if __name__ == '__main__':
     if "prepare" not in sys.argv:
         app.jinja_env.auto_reload = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
-        app.run(debug=False, host='0.0.0.0', port=port)
+        app.run(debug=True, host='0.0.0.0', port=port)
         # app.run(host='0.0.0.0', port=port)
